d3113015636eeb2af335f82d69810dc2da170d57
Manlio Morini <morinim@gmail.com>
2018-03-17 17:44:13 +0100

    [REF] Xboard related code moved to cecp.* files

---
+++ src/engine/cecp.h
@@ -0,0 +1,27 @@
+/*
+ *  This file is part of TESTUDO.
+ *
+ *  Copyright (C) 2018 Manlio Morini.
+ *
+ *  This Source Code Form is subject to the terms of the Mozilla Public
+ *  License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ *  You can obtain one at http://mozilla.org/MPL/2.0/
+ */
+
+#if !defined(TESTUDO_CECP_H)
+#define      TESTUDO_CECP_H
+
+namespace testudo
+{
+
+// CECP (aka Xboard) interface
+namespace CECP
+{
+
+void loop();
+
+}  // namespace CECP
+
+} //namespace testudo
+
+#endif  // include guard
